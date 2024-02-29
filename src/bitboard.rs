a0ade2ef2c3e965045379064b655ea1b97867c8c
Oriel Sanchez <orielbenitosanchez@gmail.com>
2024-02-29 14:53:58 -0800

    standard definition

---
+++ src/bitboard.rs
@@ -0,0 +1,15 @@
+pub struct bitboard {
+    whitePawns: u64,
+    whiteKnights: u64,
+    whiteBishops: u64,
+    whiteRooks: u64,
+    whiteQueens: u64,
+    whiteKing: u64,
+
+    blackPawns: u64,
+    blackKnights: u64,
+    blackBishops: u64,
+    blackRooks: u64,
+    blackQueens: u64,
+    blackKing: u64,
+}
